12f03e18aa726f9c59366eecf118adbe0d6fe9a8
Srajal Dixit <113223360+Srajaldixt@users.noreply.github.com>
2022-10-07 15:28:52 +0530

    Create traverse of array.c

---
+++ traverse of array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void main()
+
+{
+    int array[5],i;
+
+
+  printf("Enter array:");
+
+
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",&array[i]);
+    }
+
+      for(i=0;i<5;i++)
+    {
+        printf("%d\t",array[i]);
+    }
+
+
+
+
+}
